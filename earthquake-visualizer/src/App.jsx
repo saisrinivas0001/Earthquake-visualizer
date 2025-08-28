f7a6f670565e01529d562ce9eccdd162a9c59e82
saisrinivas000 <saisrinivas.b.offical@gmail.com>
2025-08-28 11:04:46 +0530

    app.jsx updated..!

--- earthquake-visualizer/src/App.jsx
+++ earthquake-visualizer/src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import useEarthquakeData from "./hooks/useEarthquakeData";
+import MapView from "./components/MapView";
+import StatsPanel from "./components/StatsPanel";
+import ThemeToggle from "./components/ThemeToggle";
+import Filters from "./components/Filters";
+import "./App.css";
 
 function App() {
+  const [magnitude, setMagnitude] = useState(0);
+  const [timeframe, setTimeframe] = useState("all_day");
+
+  const { earthquakes, loading, error } = useEarthquakeData(
+    `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/${timeframe}.geojson`
+  );
+
+  // Filter by magnitude
+  const filteredQuakes = earthquakes.filter(
+    (eq) => (eq.properties.mag || 0) >= magnitude
+  );
+
   return (
-    <div>this is app.jsx</div>
-  )
+    <div className="app-container">
+      {loading && <p className="loading">Loading earthquakes...</p>}
+      {error && <p className="error">Error: {error}</p>}
+
+      <MapView earthquakes={filteredQuakes} />
+      <StatsPanel earthquakes={filteredQuakes} />
+      <ThemeToggle />
+      <Filters
+        magnitude={magnitude}
+        setMagnitude={setMagnitude}
+        timeframe={timeframe}
+        setTimeframe={setTimeframe}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
